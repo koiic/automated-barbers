9b2ab7ed8b092118ba22e9b4a0ebdcb8bbc77ed9
AinaEmmanuel <ainae06@gmail.com>
2021-06-15 12:51:14 +0100

    View/Model/Urls/Serializers for adding a new saloon

--- autosaloon_main/autosaloon_api/models.py
+++ autosaloon_main/autosaloon_api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Saloon(models.Model):
+    email = models.EmailField(max_length=50)
+    name = models.CharField(max_length=30)
+    phonenumber = models.CharField(max_length=11)
+    address = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
