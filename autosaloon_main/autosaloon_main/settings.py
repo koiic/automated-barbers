d19b8ac6594cca3c1f02c0f61001bb153758d21d
Emmanuel Aina <ainae06@gmail.com>
2021-06-23 09:40:45 +0100

    Added Authentication Application

--- autosaloon_main/autosaloon_main/settings.py
+++ autosaloon_main/autosaloon_main/settings.py
@@ -40,6 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'autosaloon_api',
+    # 'auth',
+    'rest_framework_simplejwt'
+    
+
 ]
 
 MIDDLEWARE = [
@@ -129,3 +133,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES':('rest_framework.permissions.IsAuthenticatedOrReadOnly',),
+    'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework_simplejwt.authentication.JWTAuthentication',)
+}
